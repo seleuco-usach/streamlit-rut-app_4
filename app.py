8a78f806380678f3efca8c01f9bd8c48c26fdb93
xenomorfo <jose.hoyos@usach.cl>
2025-09-17 17:22:38 -0300

    filtro_carr_4

--- app.py
+++ app.py
@@ -105,7 +105,9 @@ ret_sel_carr = st.selectbox("Selecciona la retención a visualizar:",
 
 tabla_ret_largo_filtrado=tabla_ret_largo[tabla_ret_largo['variable']==ret_sel]
 
-chart_fil = alt.Chart(tabla_ret_agrupada_carr).mark_line().encode(
+tabla_ret_largo_filtrado_carr=tabla_ret_largo_carr[tabla_ret_largo_carr['variable']==ret_sel_carr]
+
+chart_fil = alt.Chart(tabla_ret_largo_filtrado_carr).mark_line().encode(
     x="ANHO_ING:O",
     y=alt.Y(ret_sel_carr, title = "Retención"),
     color="variable:N"
