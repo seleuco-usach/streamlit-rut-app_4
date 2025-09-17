d142a044db5b94f41d24e66f6554811b96f946e1
xenomorfo <jose.hoyos@usach.cl>
2025-09-17 18:09:07 -0300

    filtro_carr_10

--- app.py
+++ app.py
@@ -28,6 +28,16 @@ import numpy as np
 
 tabla_ret=pd.read_csv("tabla_ret.csv")
 
+tabla_ret['NIVEL_GLOBAL']=np.where(tabla_ret['CODIGO_CARRERA_x']=="UNICIT", "UNICIT",
+    np.where(tabla_ret['CODIGO_CARRERA_x']=="MIDA", "MAGISTER",
+    np.where(tabla_ret['CODIGO_CARRERA_x'].str[0:3]=="MAG","MAGISTER", 
+    np.where(tabla_ret['CODIGO_CARRERA_x'].str[0:3]=="DOC","DOCTORADO",
+    np.where(tabla_ret['CODIGO_CARRERA_x'].str[0:3]=="DIP","DIPLOMADO",
+    np.where(tabla_ret['CODIGO_CARRERA_x'].str[0:3]=="POS","POSTITUTLO","PREGRADO"))))))
+
+
+tabla_ret=tabla_ret[tabla_ret['NIVEL_GLOBAL']=="PREGRADO"]
+
 tabla_ret_agrupada=(tabla_ret.groupby(['ANHO_ING',])
 .agg({'ret_1': 'mean', 
       'ret_2': 'mean', 
@@ -36,7 +46,7 @@ tabla_ret_agrupada=(tabla_ret.groupby(['ANHO_ING',])
 )
 
 tabla_ret_agrupada_carr=(tabla_ret.groupby(['ANHO_ING',
-                                            'CODIGO_CARRERA_x'])
+                                            'CODIGO_CARRERA_x', 'NIVEL_GLOBAL'])
 .agg({'ret_1': 'mean', 
       'ret_2': 'mean', 
       'ret_3': 'mean'})
@@ -91,21 +101,25 @@ tabla_ret_largo=tabla_ret_agrupada.melt(id_vars=['ANHO_ING'],
              value_vars=['ret_1', 'ret_2', 'ret_3'])
 
 tabla_ret_largo_carr=tabla_ret_agrupada_carr.melt(id_vars=['ANHO_ING', 
-                                      'CODIGO_CARRERA_x'], 
+                                      'CODIGO_CARRERA_x', 'NIVEL_GLOBAL'], 
              value_vars=['ret_1', 'ret_2', 'ret_3'])
 
-ret_sel = st.radio("Selecciona la retención a visualizar:", 
-         ('ret_1', 'ret_2', 'ret_3', "todo"), index=0)
+#ret_sel = st.radio("Selecciona la retención a visualizar:", 
+ #        ('ret_1', 'ret_2', 'ret_3', "todo"), index=0)
 
 ret_sel_carr = st.selectbox("Selecciona la retención a visualizar:", 
-         (tabla_ret['CODIGO_CARRERA_x'].unique()))
+         tabla_ret['CODIGO_CARRERA_x'].unique())
+
+
+
 #st.line_chart(tabla_ret)
 
 
 
-tabla_ret_largo_filtrado=tabla_ret_largo[tabla_ret_largo['variable']==ret_sel]
+#tabla_ret_largo_filtrado=tabla_ret_largo[tabla_ret_largo['variable']==ret_sel]
 
-tabla_ret_largo_filtrado_carr=tabla_ret_largo_carr[tabla_ret_largo_carr['CODIGO_CARRERA_x']==ret_sel_carr]
+tabla_ret_largo_filtrado_carr=tabla_ret_largo_carr[(tabla_ret_largo_carr['CODIGO_CARRERA_x']==ret_sel_carr) & 
+                                                   (tabla_ret_largo_carr['NIVEL_GLOBAL']=="PREGRADO")]
 
 chart_fil = alt.Chart(tabla_ret_largo_filtrado_carr).mark_line().encode(
     x="ANHO_ING:O",
